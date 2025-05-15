5ddd41acc5476487a73bad2d844a1c6d36dafee0
João Pedro Silva <joao2255.m9@gmail.com>
2025-05-15 14:24:08 -0300

    Link WebSite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
+import { SidebarComponent } from './components/menulateral/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule, SidebarComponent],  // aqui importa RouterModule, não RouterOutlet
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ford-enter-angular';
+  showSidebar = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSidebar = event.url !== '/login';
+      }
+    });
+  }
 }
+
+
 
